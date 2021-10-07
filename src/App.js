c28a5835cb1d185a505bc2d111ff4951060e0da5
jwright90 <jason.wright90@gmail.com>
2021-10-07 14:34:17 +0100

    set max-height for hero page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Router Tutorial
+// https://www.youtube.com/watch?v=aZGzwEjZrXc&ab_channel=TheNetNinja
+
 import Navigation from './components/Navigation'
 import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <div>
-      <Navigation />
-      <Hero />
-    </div>
+    <Router>
+      <>
+        <Navigation />
+        <Switch>
+          <Route path="/">
+            <Hero />
+          </Route>
+        </Switch>
+      </>
+    </Router>
   );
 }
 
