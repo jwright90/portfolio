3e7c19c58db13d0260bbf6d56c72cfaf0e7a3662
jwright90 <jason.wright90@gmail.com>
2022-01-10 19:31:24 +0000

    fix bugs with responsive layout

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -12,7 +12,7 @@ const LandingPage = () => {
           <div className="flex flex-row text-align-left p-3">
             <div className="hero-left-section d-flex flex-column justify-content-around">
               <div className="hero-text">
-                <h1>Jason Wright</h1>
+                <h1 className="hero-title">Jason Wright</h1>
                 <Image className="hero-img-col " src={ coffee } />
                 <h3>Front End Developer</h3>
                 <h4>Bootstrap, Tailwind, Vanilla JS (ES6+), React, Next.js,</h4>
@@ -20,19 +20,23 @@ const LandingPage = () => {
                 <p>Management accountant pursuing a change of career.</p>
                 <p>In search of first web development role.</p>
               </div>
-              <div className="hero-btns">
-                <Link to="/projects">
-                  <Button className="btn-landing " variant="success" size="md">
-                    Projects              
-                  </Button>
-                </Link>
+              <Row className="hero-btns">
+                <Col sm={6}>  
+                  <Link to="/projects">
+                    <Button className="btn-landing " variant="success" size="md">
+                      Projects              
+                    </Button>
+                  </Link>
+                </Col>
+                <Col sm={6}>
                   <Button
                     href="https://docs.google.com/document/d/1Qz_cuHUk2XZPlo54GyREIULkyY99Izy0iD2OYUBX9To/edit?usp=sharing"
                     target="_blank"
                     className="btn-landing" variant="secondary" size="md">
                     CV              
                   </Button>
-              </div>
+                </Col>
+              </Row>
             </div>
 
           </div>
